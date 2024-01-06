b224fa9190a884ca3a776e51709bd1f37406adc9
ivcained <86070833+ivcained@users.noreply.github.com>
2024-01-07 01:47:33 +0530

    chore: approve scry for vain func integrated

--- vain4/components/createBounty.tsx
+++ vain4/components/createBounty.tsx
@@ -49,13 +49,13 @@ export default function CreateBounty() {
               />
               <input
                 type="text"
-                placeholder="Custom"
+                placeholder="Custom ex.0x1337"
                 value={Custom}
                 onChange={(e) => setCustom(e.target.value)}
               />
               <input
                 type="text"
-                placeholder="N Leading0s"
+                placeholder="N Leading0s ex. 0x0000, n=4 "
                 value={nLeading0s}
                 onChange={(e) => setnLeading0s(e.target.value)}
               />
@@ -65,19 +65,41 @@ export default function CreateBounty() {
                 value={flag}
                 onChange={(e) => setflag(e.target.value)}
               />
+              <p>
+                0 = user wallet EOA with n leading 0s <br />
+                1 = contract with n leading 0s <br />
+                2 = create 2 contract with n leading 0s <br />
+                3 = user wallet EOA custom <br />4 = contract custom <br />5
+                create 2 contracts
+              </p>
               <input
                 type="text"
-                placeholder="Locked"
+                placeholder="Locked Set to 0"
                 value={locked}
                 onChange={(e) => setlocked(e.target.value)}
               />
               <input
                 type="text"
-                placeholder="Amount of Scry"
+                placeholder="Amount of Scry Bounty"
                 value={amount}
                 onChange={(e) => setamount(e.target.value)}
               />
             </div>
+            <div>
+              <Web3Button
+                contractAddress="0x64ba55A341EC586A4C5d58d6297CdE5125aB55bC"
+                action={async (contract) => {
+                  const spenderAddress =
+                    "0x000000000001F04A9533e92d7AD4dDe7DC19a8F3";
+                  const amount = 10000;
+                  await contract.erc20.setAllowance(spenderAddress, amount);
+                  alert("Scry Approved for Vain");
+                }}
+              >
+                Approve SCRY for VAIN
+              </Web3Button>
+            </div>
+            <br />
             <Web3Button
               contractAddress={CONTRACT_ADDRESS}
               action={(contract) =>
